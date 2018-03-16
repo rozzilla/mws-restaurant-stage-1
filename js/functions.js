6cc85849e4073239bc2c010510a27117e9dabc34
rozzilla <roberto.bianki@gmail.com>
2018-03-17 00:17:49 +0100

    feat: Add in the cache visited page's elements

--- js/functions.js
+++ js/functions.js
@@ -2,9 +2,9 @@
  * Common functions for both index.html and restaurant.html files.
  */
 
-/* SW Registration */
+// I register the SW only if the browser support this feature
 if('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js').then(function() {
-		console.log("Service Worker Registered on /js/sw.js");
+		console.log("Service Worker Registered on /sw.js");
 	});
 }
